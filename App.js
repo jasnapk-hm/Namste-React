88ca7c3e1fdaf709228c709867fd5f908686448f
jasnapk-hm <hmjasnapk@gmail.com>
2024-09-19 14:56:34 +0530

    second episode code

--- App.js
+++ App.js
@@ -1,3 +1,19 @@
-const heading=React.createElement("h1",{},"Hello world from react");
-    const root=ReactDOM.createRoot(document.getElementById('root'));
-    root.render(heading);
+// const heading = React.createElement("h1", {}, "Hello world from react");
+
+import React from "react";
+
+import ReactDOM from "react-dom"
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Iam h1 tag ssssssssssss ")
+  ),
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Iam h1 tag")
+  ),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
